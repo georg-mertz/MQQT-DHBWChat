8666292e0b4b3b3e512d7f8539ff227db864017a
Florian <flintstoneDHBW@web.de>
2023-04-20 23:23:06 +0200

    Changed Communicator.java to Transmitter.java and added Receiver.java Works

--- src/main/java/MQTTCom/Transmitter.java
+++ src/main/java/MQTTCom/Transmitter.java
@@ -1,3 +1,4 @@
+package MQTTCom;
 
 import com.hivemq.client.mqtt.datatypes.MqttQos;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
@@ -7,26 +8,39 @@ import org.json.JSONObject;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
-public class Communicator {
+public class Transmitter {
     private final String broker;
-    private final String default_topic;
+    private final String defaultTopic;
     private final String sender;
     private final String clientID;
+    private final String clientStateTopic = "/aichat/clientstate";
+    private final String startMessage = "MQTT Chat-Client started";
+    private final String stopmessage = "MQTT Chat-Client stopped";
+    private final String lastWillMessage = "MQTT Chat-Client lost connection";
     private Mqtt5BlockingClient client;
 
-    public Communicator(String broker, String default_topic, String sender){
+
+    public Transmitter(String broker, String defaultTopic, String sender){
 
         this.broker = broker;
-        this.default_topic = default_topic;
+        this.defaultTopic = defaultTopic;
         this.sender = sender;
         this.clientID = generateGUID();
         connect();
+        sendStartMessage();
 
     }
     public boolean connect(){
         try{
             System.out.println("Trying to Connect ...");
             client = Mqtt5Client.builder().identifier(clientID).serverHost(broker).buildBlocking();
+            client.connectWith()
+                    .willPublish()
+                        .topic(clientStateTopic)
+                        .qos(MqttQos.AT_LEAST_ONCE)
+                        .payload(lastWillMessage.getBytes(StandardCharsets.UTF_8))
+                        .applyWillPublish()
+                    .send();
             return true;
         }catch (Exception e){
             e.printStackTrace();
@@ -37,9 +51,7 @@ public class Communicator {
 
         String JSONMessage = messageToJSONString(message);
         try{
-            client.connect();
-            client.publishWith().topic(default_topic).qos(MqttQos.AT_LEAST_ONCE).payload(JSONMessage.getBytes(StandardCharsets.UTF_8)).send();
-            //client.disconnect();
+            client.publishWith().topic(defaultTopic).qos(MqttQos.AT_LEAST_ONCE).payload(JSONMessage.getBytes(StandardCharsets.UTF_8)).send();
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -47,6 +59,7 @@ public class Communicator {
     }
 
     public boolean disconnect(){
+        client.publishWith().topic(clientStateTopic).qos(MqttQos.AT_LEAST_ONCE).payload(stopmessage.getBytes(StandardCharsets.UTF_8)).send();
         client.disconnect();
         return true;
     }
@@ -55,7 +68,7 @@ public class Communicator {
         jsonObject.put("sender",sender);
         jsonObject.put("text",message);
         jsonObject.put("clientId",clientID);
-        jsonObject.put("topic",default_topic);
+        jsonObject.put("topic", defaultTopic);
         return jsonObject.toString();
 
     }
@@ -63,4 +76,9 @@ public class Communicator {
     private String generateGUID(){
         return UUID.randomUUID().toString();
     }
+    private void sendStartMessage(){
+        client.publishWith().topic(clientStateTopic).qos(MqttQos.AT_LEAST_ONCE).payload(startMessage.getBytes(StandardCharsets.UTF_8)).send();
+    }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,23 @@
+import MQTTCom.Receiver;
+import MQTTCom.Transmitter;
+
 public class Main {
-    public static void main(String[] args) {
-        Communicator communicator = new Communicator("10.50.12.150","/aichat/default","SenderName");
-        System.out.println(communicator.connect());
-        System.out.println(communicator.sendMessage("TestMessage"));
-        System.out.println(communicator.disconnect());
+    public static void main(String[] args) throws InterruptedException {
+        Main main = new Main();
+        main.simulateCommunication();
+    }
+    private void simulateCommunication() throws InterruptedException {
+        Receiver receiver = new Receiver("10.50.12.150","/aichat/default");
+        Thread receiverThread = new Thread(receiver);
+        receiver.run();
+        Transmitter transmitter = new Transmitter("10.50.12.150","/aichat/default","SenderName");
+        System.out.println("Connect");
+        System.out.println(transmitter.connect());
+        System.out.println("Send Message");
+        System.out.println(transmitter.sendMessage("TestMessage"));
+        Thread.sleep(20000);
+        System.out.println("Disconnect");
+        System.out.println(transmitter.disconnect());
+        receiver.stop();
     }
 }
